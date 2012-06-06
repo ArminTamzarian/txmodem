5d5878e05016b122020c889469f09c26ff78eede
ArminTamzarian <tamzarian1989@gmail.com>
2012-06-06 11:25:39 -0500

    Reconfiguration for python module packaging.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='TXMODEM',
+    version='1.0',
+    packages=['txmodem',],
+    license='MIT license',
+    long_description=open('README.md').read(),
+)
--- txmodem/txmodem.py
+++ txmodem/txmodem.py
@@ -1,33 +1,9 @@
-#! /usr/bin/python
-
-# Copyright (c) 2012 Armin Tamzarian
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+#!/usr/bin/env python
 #
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
+# A Python class implementing the XMODEM and XMODEM-CRC send protocol.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
-#
-# XMODEM file transfer utility
-#
-# Revision History:
-# -----------------
-# 1.0 - Initial release
-
-__version__ = "1.0"
-__license__ = "MIT"
+# (C) 2012 Armin Tamzarian
+# This software is distributed under a free software license, see LICENSE
 
 import getopt
 import math
@@ -92,8 +68,6 @@ class TXMODEM:
 
     _port = None
     _checksum = None
-    
-    _callback_update = None
     
     def __init__(self, **configuration):
         """
